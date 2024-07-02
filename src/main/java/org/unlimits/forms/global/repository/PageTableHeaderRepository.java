6b6c3e7ffb21ea7a4b491f13557d970d84fb4c99
Ram Kishor <ramkishor0509@gmail.com>
2024-07-02 10:27:27 +0530

    ok

--- src/main/java/org/unlimits/forms/global/repository/PageTableHeaderRepository.java
+++ src/main/java/org/unlimits/forms/global/repository/PageTableHeaderRepository.java
@@ -3,7 +3,6 @@ package org.unlimits.forms.global.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.unlimits.forms.global.entities.EOPageTable;
 import org.unlimits.forms.global.entities.EOPageTableHeader;
 
 @Repository
